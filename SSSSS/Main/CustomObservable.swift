3a6212d07fd44cacc01b5775b1cde9d793fc92ba
Hajime <itorg110@gmail.com>
2025-12-10 18:55:41 +0900

    asyncstream導入

--- SSSSS/Main/CustomObservable.swift
+++ SSSSS/Main/CustomObservable.swift
@@ -1,19 +1,12 @@
 import Foundation
 
-enum Observable<T> {
-    case just(T)
-    case async(() async -> T)
-    case concat([Observable<T>])
-}
-
 @MainActor
 protocol CustomObservable: ObservableObject {
     associatedtype Action
-    associatedtype Mutation: Sendable
+    associatedtype Mutation
     associatedtype State
 
-    func send(_ action: Action)
-    func mutate(action: Action) async -> Observable<Mutation>
+    func mutate(action: Action, state: State) -> AsyncStream<Mutation>
     func reduce(state: inout State, mutation: Mutation)
 
     var state: State { get set }
@@ -21,36 +14,11 @@ protocol CustomObservable: ObservableObject {
 
 extension CustomObservable {
     func send(_ action: Action) {
+        // アクション発火時のStateのスナップショット
+        let currentState = self.state
         Task {
-            let mutation = await mutate(action: action)
-            var state = self.state
-            await run(state: &state, observable: mutation)
-            self.state = state
-        }
-    }
-
-    private func run(state: inout State, observable: Observable<Mutation>) async {
-        switch observable {
-        case let .just(mutation):
-            reduce(state: &state, mutation: mutation)
-
-        case let .async(task):
-            // update self.state before async
-            self.state = state
-            let mutation = await task()
-            reduce(state: &state, mutation: mutation)
-
-        case let .concat(observables1):
-            for observable1 in observables1 {
-                switch observable1 {
-                case .just, .async:
-                    await run(state: &state, observable: observable1)
-
-                case let .concat(observables2):
-                    for observable2 in observables2 {
-                        await run(state: &state, observable: observable2)
-                    }
-                }
+            for await mutation in mutate(action: action, state: currentState) {
+                reduce(state: &self.state, mutation: mutation)
             }
         }
     }
