4d8358f19b7852499e184659eb7ad7fff00e218f
Shingo Fukuyama <houseshou@gmail.com>
2025-12-10 11:54:04 +0900

    UIKit ver POC

---
+++ SSSSS/AAViewController.swift
@@ -0,0 +1,54 @@
+//
+//  AAViewController.swift
+//  SSSSS
+//
+//  Created by Shingo Fukuyama on 2025/12/10.
+//
+
+import SwiftUI
+import UIKit
+import Combine
+
+final class AAViewController: UIViewController {
+    private let viewStore = CustomViewState()
+
+    private var cancellables = Set<AnyCancellable>()
+
+    private lazy var countLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 60))
+        label.text = "Count: 0"
+        label.textColor = .white
+        label.isUserInteractionEnabled = true
+        label.textAlignment = .center
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapCountLabel))
+        label.addGestureRecognizer(tap)
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+
+        view.addSubview(countLabel)
+        countLabel.center = CGPoint(x: view.center.x, y: 100)
+
+        bindViewState()
+    }
+
+    private func bindViewState() {
+        viewStore.$state
+            .receive(on: RunLoop.main)
+            .sink { [weak self] state in
+                guard let self else { return }
+                // Example: update UI
+                if let label = self.view.subviews.compactMap({ $0 as? UILabel }).first {
+                    label.text = "Count: \(state.count)"
+                }
+            }
+            .store(in: &cancellables)
+    }
+
+    @objc private func didTapCountLabel() {
+        viewStore.send(.incrementCount)
+    }
+}
--- SSSSS/ViewController.swift
+++ SSSSS/ViewController.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     private let viewStore = CustomViewState()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .orange
 
+        setupCustomView()
+        // setupAAView()
+    }
+
+    private func setupCustomView() {
         let rootView = CustomView(store: viewStore)
         let vc = UIHostingController(rootView: rootView)
         vc.willMove(toParent: self)
@@ -24,5 +29,14 @@ class ViewController: UIViewController {
         vc.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         vc.didMove(toParent: self)
     }
-}
 
+    private func setupAAView() {
+        let vc = AAViewController()
+        vc.willMove(toParent: self)
+        view.addSubview(vc.view)
+        addChild(vc)
+        vc.view.frame = view.bounds
+        vc.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        vc.didMove(toParent: self)
+    }
+}
